bc59cae6324360336434814a8f940c1e94ce546c
wj-mi <997457785@qq.com>
2018-03-09 07:34:42 -0800

    add unittest

--- abb/abb/test/functional_test.py
+++ abb/abb/test/functional_test.py
@@ -2,10 +2,23 @@
 # @autor: wangjuan
 # @time: 2018-03-09:23:00
 
-
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://127.0.0.1:8000')
 
-assert 'Django' in browser.title
+class newTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_django(self):
+        self.browser.get('http://127.0.0.1:8000')
+
+        assert 'Django' in self.browser.title
+
+
+if __name__ == '__main__':
+    unittest.main()
